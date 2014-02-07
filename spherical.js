73c1a8b0bbb681379c3c0e425e5f24b33643c8f0
Chris Bateman <chrisb808@gmail.com>
2014-02-07 01:14:24 -0500

    formatting fix

--- spherical.js
+++ spherical.js
@@ -1,18 +1,18 @@
 /* global Impetus */
 
 window['Spherical'] = function(cfg) {
-    'use strict';
-    
+	'use strict';
+	
 	var _container;
 	var _cube;
 	var _cubeZ;
 	var _cubeRotateX = 0;
 	var _cubeRotateY = 0;
-    var _imp;
-    var _enableFullscreen = true;
-    var _isFullscreen = false;
-    var _fsButton;
-    var _supportsFullscreenAPI = !!document.webkitCancelFullScreen || !!document.mozCancelFullScreen || !!document.cancelFullScreen;
+	var _imp;
+	var _enableFullscreen = true;
+	var _isFullscreen = false;
+	var _fsButton;
+	var _supportsFullscreenAPI = !!document.webkitCancelFullScreen || !!document.mozCancelFullScreen || !!document.cancelFullScreen;
 	
 	var _cubeTemplate = '<div class="spherical-cube"><div class="spherical-front" style="background-image:url({{fImg}});"></div><div class="spherical-back" style="background-image:url({{bImg}});"></div><div class="spherical-left" style="background-image:url({{lImg}});"></div><div class="spherical-right" style="background-image:url({{rImg}});"></div><div class="spherical-top" style="background-image:url({{uImg}});"></div><div class="spherical-bottom" style="background-image:url({{dImg}});"></div></div>';
 	
@@ -50,45 +50,45 @@ window['Spherical'] = function(cfg) {
 		_container.style.webkitPerspective = _container.style.perspective = _cubeZ + 'px';
 	};
 	
-    var _increaseZoom = function(amt) {
-        amt /= 15;
-        amt = ((0.2 * _cubeZ) - 8.2) * amt;
-        
-        amt = _cubeZ + amt;
-        
-        _setCubeZ(amt);
-    };
-    
+	var _increaseZoom = function(amt) {
+		amt /= 15;
+		amt = ((0.2 * _cubeZ) - 8.2) * amt;
+		
+		amt = _cubeZ + amt;
+		
+		_setCubeZ(amt);
+	};
+	
 	var _setCubeZ = function(z) {
 		_cubeZ = Math.max(Math.min(1500, z), 130);
-        _updateCubeTransform();
-        _updatePerspective();
+		_updateCubeTransform();
+		_updatePerspective();
 	};
-    
-    var _addZoomControls = function() {
-        var _onScroll = function(ev) {
-            var delta = Math.max(-1, Math.min(1, (ev.wheelDelta || -ev.detail)));
-            //document.getElementById('logger').innerHTML = delta;
-            _increaseZoom(delta * 15);
-            //_setCubeZ(_cubeZ + (delta * 15));
-        };
-        document.addEventListener('mousewheel', _onScroll);
-        document.addEventListener('DOMMouseScroll', _onScroll);
-        
-        
-        // TEMPORARY
-        var _lastScale = 1;
-        var _onGestureChange = function(ev) {
-            //document.getElementById('logger').innerHTML = ev.scale.toFixed(2) + ' | ' + (ev.scale - _lastScale).toFixed(2);
-            //document.getElementById('logger').innerHTML = ev.scale.toFixed(2) + ' - ' + _cubeZ.toFixed(1);
-            
-            _increaseZoom((ev.scale - _lastScale) * 50);
-            //_setCubeZ(_cubeZ += (ev.scale - _lastScale) * 350);
-            //_setCubeZ(_cubeZ *= ev.scale);
-            
-            _lastScale = ev.scale;
-        };
-        document.body.addEventListener('gesturestart', function(e) {
+	
+	var _addZoomControls = function() {
+		var _onScroll = function(ev) {
+			var delta = Math.max(-1, Math.min(1, (ev.wheelDelta || -ev.detail)));
+			//document.getElementById('logger').innerHTML = delta;
+			_increaseZoom(delta * 15);
+			//_setCubeZ(_cubeZ + (delta * 15));
+		};
+		document.addEventListener('mousewheel', _onScroll);
+		document.addEventListener('DOMMouseScroll', _onScroll);
+		
+		
+		// TEMPORARY
+		var _lastScale = 1;
+		var _onGestureChange = function(ev) {
+			//document.getElementById('logger').innerHTML = ev.scale.toFixed(2) + ' | ' + (ev.scale - _lastScale).toFixed(2);
+			//document.getElementById('logger').innerHTML = ev.scale.toFixed(2) + ' - ' + _cubeZ.toFixed(1);
+			
+			_increaseZoom((ev.scale - _lastScale) * 50);
+			//_setCubeZ(_cubeZ += (ev.scale - _lastScale) * 350);
+			//_setCubeZ(_cubeZ *= ev.scale);
+			
+			_lastScale = ev.scale;
+		};
+		document.body.addEventListener('gesturestart', function(e) {
 			_imp.pause();
 			_lastScale = 1;
 		});
@@ -96,44 +96,44 @@ window['Spherical'] = function(cfg) {
 		document.body.addEventListener('gestureend', function(e) {
 			_imp.unpause();
 		});
-    };
-    
-    this.toggleFullscreen = function() {
-        if (_isFullscreen) {
-            if (_supportsFullscreenAPI) {
-                document.webkitExitFullscreen();
-            } else {
-                _container.className = 'spherical-container';
-                _isFullscreen = !_isFullscreen;
-            }
-        } else {
-            if (_supportsFullscreenAPI) {
-                _container.webkitRequestFullscreen();
-                _fsButton.style.display = 'none';
-            } else {
-                _container.className = 'spherical-container spherical-pseudo-fs';
-                _isFullscreen = !_isFullscreen;
-            }
-        }
-        
-    };
-    
-    var _onFullscreenChange = function() {
-        this.resetZ();
-        _isFullscreen = !_isFullscreen;
-        
-        if (!document.webkitFullscreenElement && _supportsFullscreenAPI) {
-            _fsButton.style.display = '';
-        }
-    }.bind(this);
-    
-    
-    this.resetZ = function() {
-        _cubeZ = (Math.sqrt(_container.clientWidth * _container.clientHeight) / 2.5);
+	};
+	
+	this.toggleFullscreen = function() {
+		if (_isFullscreen) {
+			if (_supportsFullscreenAPI) {
+				document.webkitExitFullscreen();
+			} else {
+				_container.className = 'spherical-container';
+				_isFullscreen = !_isFullscreen;
+			}
+		} else {
+			if (_supportsFullscreenAPI) {
+				_container.webkitRequestFullscreen();
+				_fsButton.style.display = 'none';
+			} else {
+				_container.className = 'spherical-container spherical-pseudo-fs';
+				_isFullscreen = !_isFullscreen;
+			}
+		}
+		
+	};
+	
+	var _onFullscreenChange = function() {
+		this.resetZ();
+		_isFullscreen = !_isFullscreen;
+		
+		if (!document.webkitFullscreenElement && _supportsFullscreenAPI) {
+			_fsButton.style.display = '';
+		}
+	}.bind(this);
+	
+	
+	this.resetZ = function() {
+		_cubeZ = (Math.sqrt(_container.clientWidth * _container.clientHeight) / 2.5);
 		
 		_updateCubeTransform();
 		_updatePerspective();
-    };
+	};
 	
 	
 	(function init() {
@@ -168,17 +168,17 @@ window['Spherical'] = function(cfg) {
 			}
 		});
 		
-        _addZoomControls();
+		_addZoomControls();
 		
 		if (_enableFullscreen && _supportsFullscreenAPI) {
-            _fsButton = document.createElement('button');
-            _fsButton.className = 'spherical-fullbutton';
-            _fsButton.onclick = this.toggleFullscreen;
-            _container.appendChild(_fsButton);
-            
-           //_container.addEventListener('dblclick', this.toggleFullscreen);
-            
-            _container.onwebkitfullscreenchange = _container.onmozfullscreenchange = _container.onfullscreenchange = _onFullscreenChange;
+			_fsButton = document.createElement('button');
+			_fsButton.className = 'spherical-fullbutton';
+			_fsButton.onclick = this.toggleFullscreen;
+			_container.appendChild(_fsButton);
+			
+		   //_container.addEventListener('dblclick', this.toggleFullscreen);
+			
+			_container.onwebkitfullscreenchange = _container.onmozfullscreenchange = _container.onfullscreenchange = _onFullscreenChange;
 		}
 		
 	}).bind(this)();
